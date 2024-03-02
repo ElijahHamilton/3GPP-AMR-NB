413eb648d48d7f2b93f5ec6e73f99eb3628df3ea
Elijah Hamilton <elijah.w.hamilton@gmail.com>
2024-03-02 09:18:14 -0600

    Update build-fast.sh

--- build-fast.sh
+++ build-fast.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc -Icodec/ -O3 -DVAD1 progams/encoder.c codec/*enc.c -o encoder
-gcc -Icodec/ -O3 -DVAD1 progams/decoder.c codec/*dec.c -o decoder
+gcc -Icodec/ -O3 -DVAD1 progams/encoder.c codec/*enc.c -o encoder -lm
+gcc -Icodec/ -O3 -DVAD1 progams/decoder.c codec/*dec.c -o decoder -lm
